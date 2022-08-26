dcbb23b2ee3d898bb49db39ffa7ebbff09c4bee3
Jatin9207 <78591702+Jatin9207@users.noreply.github.com>
2022-08-26 10:40:42 +0530

    Add files via upload

---
+++ Jatin 2010990338 2nd code.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void findRepeat(int* a, int n){
+    
+    unordered_map<int,int> hash;
+    
+    for(int i=0;i<n;i++){
+        hash[a[i]]++;
+    }
+    
+    cout<<"repeated number      frequency\n";
+    
+    for(auto it=hash.begin();it!=hash.end();it++)
+    if(it->second>1)
+    printf("%d\t\t\t%d\n",it->first,it->second);
+    
+}
+
+int main()
+{
+    int n;
+    cout<<"enter array length\n";
+    cin>>n;
+    int* a=(int*)(malloc(sizeof(int)*n));
+    
+    cout<<"input array elements...\n";
+    
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    
+    findRepeat(a,n);
+
+    return 0;
+}
